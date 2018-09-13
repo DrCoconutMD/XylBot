4c828a1948c609bcfd4aa0d02e1b80f02c1c8749
Jens Olesen <jensolesen47@gmail.com>
2018-09-12 21:09:51 -0400

    Improve dice.roll to allow multiple arguments

--- dice/dice.js
+++ dice/dice.js
@@ -1,20 +1,22 @@
-module.exports.roll = args => {
-    let arg = args[0];
-    if (!arg) {
-        arg = `1d6`;
+module.exports.roll = (channel, user, args) => {
+    if (!args[0]) {
+        args.push(`1d6`);
     }
-    const dice = deduceDice(arg);
-
-    const numberOfDice = dice[0] ? dice[0] : 1;
-    const sidesOfDice = dice[1] ? dice[1] : 6;
-
     const rolledDice = [];
-    for (let i = 0; i < numberOfDice; i++) {
-        rolledDice.push(Math.floor(Math.random() * sidesOfDice + 1));
+    
+    while (args.length > 0) {
+        const dice = deduceDice(args.shift());
+
+        const numberOfDice = dice[0] ? dice[0] : 1;
+        const sidesOfDice = dice[1] ? dice[1] : 6;
+
+        for (let i = 0; i < numberOfDice; i++) {
+            rolledDice.push(Math.floor(Math.random() * sidesOfDice + 1));
+        }
     }
     const total = rolledDice.reduce((a, b) => a + b, 0);
 
-    return `You rolled a ${total} (${rolledDice.join(', ')}).`;
+    channel.send(`Hey ${user.username}, You rolled a ${total} (${rolledDice.join(', ')}).`);
 };
 
 const deduceDice = dice => {
